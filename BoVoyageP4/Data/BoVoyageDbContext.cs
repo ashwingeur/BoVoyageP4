f368e10490330e2fb6feda9402e7c42751bd1732
stephanecha <schamblain@gmail.com>
2018-10-09 12:33:28 +0200

    Ajout de la chaine de connection et BoVoyageDbContext

---
+++ BoVoyageP4/Data/BoVoyageDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BoVoyageP4.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageP4.Data
+{
+    public class BoVoyageDbContext : DbContext
+    {
+        public BoVoyageDbContext() : base("BoVoyageConnectionString")
+        {
+        }
+
+        public DbSet<AgenceVoyage> AgencesVoyages { get; set; }
+
+        public DbSet<Destination> Destinations { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Assurance> Assurances { get; set; }
+
+        public DbSet<DossierReservation> DossierReservations { get; set; }
+
+        public DbSet<Participant> Participants { get; set; }
+
+        public DbSet<Voyage> Voyages { get; set; }
+
+    }
+
+
+
+}
