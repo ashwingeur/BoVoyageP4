4a6ccfaff05c5321c278701994f5c16f3ce9edc3
George Doggo <volty0@orange.fr>
2018-10-10 11:30:58 +0200

    Base Controller + outils

---
+++ BoVoyageP4/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using BoVoyageP4.Data;
+using BoVoyageP4.Outils;
+using System.Web.Mvc;
+
+namespace BoVoyageP4.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected BoVoyageDbContext db = new BoVoyageDbContext();
+
+        protected void Display(string text, MessageType type = MessageType.SUCCESS)
+        {
+            var m = new Message(type, text);
+            TempData["MESSAGE"] = m;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (!disposing)
+                this.db.Dispose();
+        }
+    }
+}
