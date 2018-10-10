1adab9781fb1394455229f6ba12442d9c288a31a
ashwingeur <january05@hotmail.fr>
2018-10-10 11:32:44 +0200

    Creation authentification controllers

---
+++ BoVoyageP4/Areas/BackOffice/Controllers/AuthentificationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageP4.Areas.BackOffice.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        // GET: BackOffice/Authentification
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
