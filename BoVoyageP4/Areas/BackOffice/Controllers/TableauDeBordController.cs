3a6481b12d49301ad72740ec765c5fd4477fe371
stephanecha <schamblain@gmail.com>
2018-10-12 14:27:37 +0200

    Ajout Tableau de bord

--- BoVoyageP4/Areas/BackOffice/Controllers/TableauDeBordController.cs
+++ BoVoyageP4/Areas/BackOffice/Controllers/TableauDeBordController.cs
@@ -1,10 +1,13 @@
 ﻿using BoVoyageP4.Controllers;
 using BoVoyageP4.Filters;
+using BoVoyageP4.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+...
 
 namespace BoVoyageP4.Areas.BackOffice.Controllers
 {
@@ -14,7 +17,14 @@ namespace BoVoyageP4.Areas.BackOffice.Controllers
         // GET: BackOffice/TableauDeBord
         public ActionResult Index()
         {
-            return View();
+            var voyages = db.Voyages.Include("Destination").Where(x=>x.DateAller < DbFunctions.AddDays(DateTime.Now, 15)).ToList();
+            var dossiers = db.DossierReservations.Include("Client").Where(x=>x.EtatDossierReservation == 0).ToList();
+            TableauDeBord dashboard = new TableauDeBord()
+            {
+                Voyage = voyages,
+                DossierReservation = dossiers
+            };
+            return View(dashboard);
         }
     }
 }
